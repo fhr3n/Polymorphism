488ef55fbc2eed7bc348d21bd0bc9c637f107218
fhr3n <ridhofahren@gmail.com>
2020-05-08 22:55:55 +0700

    test

---
+++ Polymorphism/Canon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Canon : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("\nCanon Display Dimension : 9.5*12");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Canon Printer Printing....");
+        }
+    }
+}
---
+++ Polymorphism/PrinterWindows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class PrinterWindows
+    {
+        public string nama = "Printer";
+        public string dimensi = "15x15";
+
+        public virtual void Show()
+        {
+            Console.WriteLine(nama + " Display Dimension : " + dimensi);
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine(nama + " Printer Printing....");
+        }
+    }
+}
